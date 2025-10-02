3df2b7173b606a9d2a0e67fd81ba11f88bdbe7cd
ebraheem hany <ebraheemhany2004@gmail.com>
2025-10-02 15:21:27 +0300

    Update: آخر التعديلات اللي عملتها

--- src/Component/Cards/Cards.jsx
+++ src/Component/Cards/Cards.jsx
@@ -8,48 +8,42 @@ import { BigCard } from "../BigCard/BigCard";
 import { Link } from "react-router";
 
 export const Cards = ({ title, data , more }) => {
-  var settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 5,
-    slidesToScroll: 2,
-    initialSlide: 0,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    pauseOnHover: true,
-    arrows: false,
-    responsive: [
-      {
-        breakpoint: 1420,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 1,
-          infinite: true,
-        },
+var settings = {
+  dots: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 4, // خليه 4 بدل 5 كبداية أساسية
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  pauseOnHover: true,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 1420,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
       },
-      {
-        breakpoint: 1208,
-        settings: {
-           slidesToShow: 3,
-            slidesToScroll: 1,
-             initialSlide: 2 
-            },
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
       },
-      { 
-        breakpoint: 900,
-         settings: { 
-          slidesToShow: 2,
-           slidesToScroll: 1 
-          } },
-      { 
-        breakpoint: 500,
-         settings: { 
-          slidesToShow: 1,
-   
-          } },
-    ],
-  };
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
+
 
   return (
     <div className="cards  ">
